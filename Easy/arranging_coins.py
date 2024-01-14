b6eb3d9bb7e5e451b2c441cfd81c20879a195538
Nikhil Kotra <84204563+nikhil-1e9@users.noreply.github.com>
2024-01-14 12:18:44 +0530

    Create arranging_coins.py

---
+++ Easy/arranging_coins.py
@@ -0,0 +1,7 @@
+class Solution:
+  """
+  You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+  Given the integer n, return the number of complete rows of the staircase you will build.
+  """
+    def arrangeCoins(self, n: int) -> int:
+        return int((1+8*n)**0.5 - 1) // 2
