11dee2f0b9ba627e387137885eed15eb9c55ee0b
Nikhil Kotra <84204563+nikhil-1e9@users.noreply.github.com>
2024-02-11 14:14:31 +0530

    Update string_permutation.py

--- Medium/string_permutation.py
+++ Medium/string_permutation.py
@@ -1,6 +1,8 @@
 class Solution:
   """
-  
+  Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+  In other words, return true if one of s1's permutations is the substring of s2.
   """
   ## ---------- Brute Force -> O(n^2*logn) time, O(1) space----------- ##
     def checkInclusion(self, s1: str, s2: str) -> bool:
